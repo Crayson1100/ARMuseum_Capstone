0cf675ff517f94c59f85110232d284d899eb8742
Ethan Anderson <ECA9001@gmail.com>
2026-02-12 21:22:10 -0700

    Update Exhibit, ScaleModel Method made
    ScaleModel should keep the models at least somewhat within scale of our tables and then we can address other scaling issues.

--- Assets/Scripts/Ethan/Exhibit.cs
+++ Assets/Scripts/Ethan/Exhibit.cs
@@ -15,14 +15,54 @@ public class Exhibit : MonoBehaviour
 
     public ArtData art;
     public Transform localPosition;
+    private GameObject displayObject;
 
     public Transform[] artLocation;
 
 
     private void Start()
     {
-        var obj = Instantiate<GameObject>(art.model, artLocation[0].position, Quaternion.identity);
+        displayObject = Instantiate<GameObject>(art.model, artLocation[0].position, Quaternion.identity);
+        ScaleModel(displayObject.transform);
+       
+
+    }
+
+    /// <summary>
+    /// Scales a given models renderers to the same size as our exhibit renderers
+    /// </summary>
+    /// <param name="model"> the transform of the model</param>
+    private void ScaleModel(Transform model)
+    {
+        Renderer[] modelRenderers = model.GetComponentsInChildren<Renderer>();
+        Renderer[] tableRenderers = GetComponentsInChildren<Renderer>();
+
+        if (tableRenderers.Length == 0 || tableRenderers == null) return;
+        if (modelRenderers.Length == 0 || modelRenderers == null) return;
+
+        model.localScale = Vector3.one;
+
+        Bounds modelBounds = modelRenderers[0].bounds;
+        foreach (Renderer r in modelRenderers)
+        {
+            modelBounds.Encapsulate(r.bounds);
+        }
 
 
+        Bounds tableBounds = tableRenderers[0].bounds;
+        foreach (Renderer r in tableRenderers)
+        {
+            tableBounds.Encapsulate(r.bounds);
+        }
+
+        Vector3 objectSize = modelBounds.size;
+        Vector3 tableSize = tableBounds.size;
+
+        float scaleX = tableSize.x / objectSize.x;
+        float scaleZ = tableSize.z / objectSize.z;
+
+        float scaleFactor = Mathf.Min(scaleX, scaleZ);
+
+        model.localScale *= scaleFactor;
     }
 }
